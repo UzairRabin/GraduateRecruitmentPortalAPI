ed4d5c9e830e49df3c79a8fb11801442684c41c0
miketyo <mikesomelezotyolani41@gmail.com>
2022-11-24 19:08:33 +0200

    Created ExperienceController.

---
+++ src/main/java/za/ac/cput/controller/ExperienceController.java
@@ -0,0 +1,58 @@
+package za.ac.cput.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import za.ac.cput.factory.ExperienceFactory;
+import za.ac.cput.model.Experience;
+import za.ac.cput.service.experience.impl.ExperienceServiceImpl;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+//@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@RequestMapping("api/v1/graduate-portal/experience/")
+public class ExperienceController {
+    private final ExperienceServiceImpl experienceService;
+
+    @Autowired
+    public ExperienceController(ExperienceServiceImpl experienceService) {
+        this.experienceService = experienceService;
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<Experience> save(@Valid @RequestBody Experience experience) {
+        Experience newExperience = ExperienceFactory.build(experience.getExperienceId(), experience.getJobTitle(), experience.getAssumedRole(), experience.getStartDate(), experience.getEndDate());
+        Experience experienceSaved = this.experienceService.save(newExperience);
+        return ResponseEntity.ok(experienceSaved);
+    }
+
+    @GetMapping("read/{id}")
+    public ResponseEntity<Experience> read(@PathVariable String id) {
+        Experience readExperience= this.experienceService.read(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Experience not found"));
+        return ResponseEntity.ok(readExperience);
+    }
+
+    @DeleteMapping("delete")
+    public ResponseEntity<Void> delete(Experience experience) {
+        this.experienceService.delete(experience);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+        this.experienceService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<Experience>> findAll() {
+        List<Experience> findAllTeamList = this.experienceService.findAll();
+        return ResponseEntity.ok(findAllTeamList);
+    }
+
+}
