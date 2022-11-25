93df654aad49ad389d5144d34fc1cab19d9d9df3
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-11-25 11:18:49 +0200

    TASK: Added a new department repository class

---
+++ src/main/java/za/ac/cput/repository/IDepartmentRepository.java
@@ -0,0 +1,16 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.model.Department;
+
+import java.util.Optional;
+
+/**
+ * @author Chadrack Mbuyi
+ * */
+
+@Repository
+public interface IDepartmentRepository extends JpaRepository<Department, Integer> {
+    Optional<Department> findExperienceByExperienceId(int departmentId);
+}
