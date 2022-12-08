62b6a840ccd1d0ba401ee6777e5358b286ba4eb0
ishmail_mgwena <ishmail.mgwena24@gmail.com>
2022-12-08 03:00:13 +0200

    Final recruiter commit

---
+++ src/main/java/za/ac/cput/controller/RecruiterController.java
@@ -0,0 +1,53 @@
+package za.ac.cput.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import za.ac.cput.model.Recruiter;
+import za.ac.cput.service.IRecruiterService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/graduate-portal/recruiter/")
+@Slf4j
+public class RecruiterController {
+    private final IRecruiterService service;
+
+    @Autowired
+    RecruiterController(IRecruiterService service){this.service = service;}
+
+    @PostMapping("save")
+    public ResponseEntity<Recruiter> save(@RequestBody Recruiter recruiter){
+        Recruiter recruiterReturned = null;
+        recruiterReturned = service.save(recruiter);
+        return ResponseEntity.ok(recruiterReturned);
+    }
+
+    @GetMapping("read/recruiterId")
+    public ResponseEntity<Recruiter> read(@PathVariable long recruiterId) {
+        log.info("Read request:{}", recruiterId);
+        Recruiter read = this.service.read(recruiterId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Recruiter not found"));
+        return ResponseEntity.ok(read);
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<Recruiter>>findAll(){List<Recruiter> recruiter = this.service.findAll();
+        return ResponseEntity.ok(recruiter);}
+
+    @DeleteMapping("delete/{recruiterId}")
+    public ResponseEntity<Void> deleteById(@PathVariable long recruiterId) {service.deleteById(recruiterId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("delete")
+    public ResponseEntity<Void> delete(Recruiter recruiter) {this.service.delete(recruiter);
+        return ResponseEntity.noContent().build();
+    }
+}
+
+
---
+++ src/main/java/za/ac/cput/service/RecruiterServiceImpl.java
@@ -0,0 +1,48 @@
+package za.ac.cput.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.factory.RecruiterFactory;
+import za.ac.cput.model.Recruiter;
+import za.ac.cput.repository.IRecruiterRepository;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class RecruiterServiceImpl implements IRecruiterService{
+
+    IRecruiterRepository recruiterRepository;
+    Recruiter safeRecruiter;
+
+    @Autowired
+    public RecruiterServiceImpl(IRecruiterRepository recruiterService){this.recruiterRepository = recruiterService;}
+
+    @Override
+    public Recruiter save(Recruiter recruiter) {
+        try {
+            safeRecruiter = RecruiterFactory.build(recruiter.getRecruiterId(),
+                    recruiter.getRecruiterName(), recruiter.getCompanyName(),
+                    recruiter.getContactNumber(), recruiter.getDateAdded());
+        }
+        catch (IllegalArgumentException /*| IOException*/ exception)
+        {
+            log.error("Recruiter Service: Save Recruiter:{}", exception);
+        }
+        return recruiterRepository.save(safeRecruiter);
+        }
+        @Override
+        public Optional<Recruiter> read(Long integer){return recruiterRepository.findById(integer);}
+
+        @Override
+        public List<Recruiter> findAll(){return this.recruiterRepository.findAll();}
+
+        @Override
+        public void delete(Recruiter recruiter){this.delete(recruiter);}
+
+        @Override
+        public void deleteById(Long recruiterId){recruiterRepository.deleteById(recruiterId);}
+    }
