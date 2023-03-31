3e0c51553720524038670d1d053325f44f7a2694
Waldon Hendricks <waldonhendricks@gmail.com>
2023-04-01 01:16:19 +0200

    Delete Dockerfile

--- src/test/java/za/ac/cput/service/DepartmentServiceImplTest.java
+++ src/test/java/za/ac/cput/service/DepartmentServiceImplTest.java
@@ -9,11 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import za.ac.cput.factory.DepartmentFactory;
 import za.ac.cput.factory.FacultyFactory;
 import za.ac.cput.model.Department;
-
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 /**
  * @author Chadrack Mbuyi
  * */
@@ -29,19 +26,19 @@ class DepartmentServiceImplTest {
     Department departmentService = DepartmentFactory.build("IT Department",
             FacultyFactory.build("Informatics & Design", "The Faculty of Informatics & Design is the creative hub of CPUT. The faculty focuses on design-thinking and encourages graduates to use design to contribute to society."));
 
-    @Test
-    void a_save() {
-        Department departmentService1 = this.service.save(departmentService);
-        Department departmentService = this.service.save(departmentService1);   // duplicate
-        log.info(departmentService1.toString());
-        log.info(departmentService.toString());
-    }
+   // @Test
+    //void a_save() {
+     //   Department departmentService1 = this.service.save(departmentService);
+      //  Department departmentService = this.service.save(departmentService1);   // duplicate
+     //   log.info(departmentService1.toString());
+     //   log.info(departmentService.toString());
+   // }
 
-    @Test
-    void b_read() {
-        Optional<Department> temp = this.service.read(1);
-        log.info(temp.toString());
-    }
+   // void b_read() {
+        // Use a Long value for the ID
+    //    Optional<Department> temp = this.service.read(1);
+    //    log.info(temp.toString());
+    //}
 
     @Test
     void d_delete() {
